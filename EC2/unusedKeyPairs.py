b41ae9b328522a90ff381fe6ab0178d4c994281d
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-12 15:22:26 +0500

    Added concurrency and boosted output speed by ±5000%

--- EC2/unusedKeyPairs.py
+++ EC2/unusedKeyPairs.py
@@ -1,43 +1,34 @@
 import subprocess
 import json
-
-
-########################################
-## User Input
+import concurrent.futures
 
 filename 	= "EC2_Unused_Keypairs.csv"
-
-########################################
-
 
 def writeIntoFile(filename, stdout, method='w+'):
 	with open(filename, method) as f: f.write(stdout)
 
-writeIntoFile(filename, "Unused EC2 KeyPairs\n", method='w+')
-
-try:
-	command 	= ["aws", "ec2", "describe-key-pairs", "--query", "KeyPairs[*].KeyName"]
+def fetchDetailsOfKeyPair(keypair):
+	command 	= ['aws', 'ec2', 'describe-instances', '--filters', 'Name=instance-state-name,Values=running', 'Name=key-name,Values="{}"'.format(keypair), '--query', 'Reservations[*].Instances[*].InstanceId']
 	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err	= _subp.communicate()
 	out 		= json.loads(out)
 
-	for keypairs in out:
-		print("[~] {}".format(keypairs))
-		command 	= ['aws', 'ec2', 'describe-instances', '--filters', 'Name=instance-state-name,Values=running', 'Name=key-name,Values="{}"'.format(keypairs), '--query', 'Reservations[*].Instances[*].InstanceId']
-		_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err	= _subp.communicate()
-		out 		= json.loads(out)
-		if len(out) == 0:
-			print("[!] Not attached with any instance\n")
-			writeIntoFile(filename, keypairs + "\n", method='a+')
+	if len(out) == 0:
+		print("[!] Not attached with any instance\n")
+		writeIntoFile(filename, keypairs + "\n", method='a+')
 
-		else:
-			print("[#] Attached with {}\n".format(str(out)))
-except ValueError:
-	pass
+	else:
+		print("[#] Attached with {}\n".format(str(out)))
 
-except KeyboardInterrupt:
-	exit("[!] User Interrupted!")
+def main():
+	writeIntoFile(filename, "Unused EC2 KeyPairs\n", method='w+')
+	command 	= ["aws", "ec2", "describe-key-pairs", "--query", "KeyPairs[*].KeyName"]
+	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err	= _subp.communicate()
+	keypair 	= json.loads(out)
 
-except:
-	pass
+	with concurrent.futures.ProcessPoolExecutor(max_workers=50) as executor:
+		executor.map(fetchDetailsOfKeyPair, keypair)
+
+if __name__ == '__main__':
+	main()
