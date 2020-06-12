1c70d8fa3aced0469cd0daae48e4b51b42b80e5f
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-12 16:46:51 +0500

    Increased output speed to 5000% of the original with Python3's concurrency

--- EC2/outdatedAMIs.py
+++ EC2/outdatedAMIs.py
@@ -3,13 +3,13 @@ import subprocess
 import os
 import json
 import re
-
+import concurrent.futures
 
 ###############
 ## User Input
 
 daysToCheck	= 180
-dateToday 	= datetime(2018, 12, 18) 			# Add script execution date here Y:M:D
+dateToday 	= datetime(2020, 6, 12) 			# Add script execution date here Y:M:D
 fileName 	= "EC2_Outdated_AMIs.csv"
 
 ###############
@@ -17,39 +17,39 @@ fileName 	= "EC2_Outdated_AMIs.csv"
 def writeIntoFile(filename, stdout, method='w+'):
 	with open(filename, method) as f: f.write(stdout)
 
-print("[~] Grabbing List of AMIs (be patient)\n")
-command 	= ["aws", "ec2", "describe-images", "--owners", "self", "--query", "Images[*].ImageId"]
-_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err 	= _subp.communicate()
-out 		= json.loads(out)
+def describeAMIsDuration(imageId):
+	command 			= ["aws", "ec2", "describe-images", "--image-ids", imageId]
+	_subp 				= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err 			= _subp.communicate()
+	out 				= json.loads(out)['Images']
 
-writeIntoFile(fileName, stdout="Age (days old),EC2 Instance ID,EC2 Instance Name,EBS Volume Encryption\n", method='w+')
-print("Age ~ Instance ID ~ InstanceName ~ EbsVolumeEncryption\n")
+	for instanceDetails in out:
+		instanceName 		= instanceDetails['Name']
+		ebsVolumeEncryption	= instanceDetails['BlockDeviceMappings'][0]['Ebs']['Encrypted']
+		creationDate 		= instanceDetails['CreationDate']
 
-for imageId in out:
-	try:
-		command 			= ["aws", "ec2", "describe-images", "--image-ids", imageId]
-		_subp 				= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err 			= _subp.communicate()
-		out 				= json.loads(out)['Images']
+		date 				= creationDate.split("T")[0].split("-")
+		year, month, day 	= int(date[0]), int(date[1]), int(date[2])
+		amiAgeInDays 		= str(dateToday - datetime(year, month, day)).split(",")[0].replace(" days", "")
+		print("{:<3} ~ {} ~ {} ~ {}".format(amiAgeInDays, imageId, instanceName, str(ebsVolumeEncryption)))
 
-		for instanceDetails in out:
-			instanceName 		= instanceDetails['Name']
-			ebsVolumeEncryption	= instanceDetails['BlockDeviceMappings'][0]['Ebs']['Encrypted']
-			creationDate 		= instanceDetails['CreationDate']
+		if int(amiAgeInDays) > daysToCheck: 
+			writeIntoFile(fileName, stdout="{},{},{},{}\n".format(amiAgeInDays, imageId, instanceName, str(ebsVolumeEncryption)), method='a+')
 
-			date 				= creationDate.split("T")[0].split("-")
-			year, month, day 	= int(date[0]), int(date[1]), int(date[2])
-			amiAgeInDays 		= str(dateToday - datetime(year, month, day)).split(",")[0].replace(" days", "")
-			print("{:<3} ~ {} ~ {} ~ {}".format(amiAgeInDays, imageId, instanceName, str(ebsVolumeEncryption)))
+def main():
+	writeIntoFile(fileName, stdout="Age (days old),EC2 Instance ID,EC2 Instance Name,EBS Volume Encryption\n", method='w+')
+	print("[~] Grabbing List of AMIs (be patient)\n")
 
-			if int(amiAgeInDays) > daysToCheck: 
-				writeIntoFile(fileName, stdout="{},{},{},{}\n".format(amiAgeInDays, imageId, instanceName, str(ebsVolumeEncryption)), method='a+')
-	
-	except KeyboardInterrupt:
-		exit("[!] You killed my process :(")
+	command 	= ["aws", "ec2", "describe-images", "--owners", "self", "--query", "Images[*].ImageId"]
+	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err 	= _subp.communicate()
+	out 		= json.loads(out)
+	print("Age ~ Instance ID ~ InstanceName ~ EbsVolumeEncryption\n")
 
-	except:
-		pass
+	with concurrent.futures.ProcessPoolExecutor(max_workers=50) as executor:
+		executor.map(describeAMIsDuration, out)
 
-print("[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+	print("\n[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+
+if __name__ == '__main__':
+	main()
