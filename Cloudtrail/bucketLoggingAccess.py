8b71beff1bf29e18b3230ac5b6b987b52f091ebc
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-15 16:40:55 +0500

    Implemented output logging

--- Cloudtrail/bucketLoggingAccess.py
+++ Cloudtrail/bucketLoggingAccess.py
@@ -5,6 +5,11 @@ import botocore
 import boto3
 import json
 
+fileName 	= "bucketsWithoutLoggingAccess.csv"
+
+def writeIntoFile(filename, stdout, method='w+'):
+	with open(filename, method) as f: f.write(stdout)
+
 def authorizedClientCall():
 	client 		= boto3.client("cloudtrail")
 	return(client)
@@ -23,20 +28,24 @@ def listCloudtrailBuckets(clientApiCall):
 
 	for name, arn, _bucketName in zip(trailName, trailARN, bucketName):
 		try:
-			mfaDeletion 	= boto3.client('s3').get_bucket_logging(Bucket=_bucketName)
-			deletionStatus 	= parseJson('Status', mfaDeletion)
-			
-			if deletionStatus == None:
-				print(f"[!] {_bucketName}\t ~ {deletionStatus}\t ~ {name}\t ~ {arn}")
+			logging 		= boto3.client('s3').get_bucket_logging(Bucket=_bucketName)
+			bucketLogging 	= parseJson('Status', logging)
+
+			writeIntoFile(fileName, stdout="{},{},{},{}\n".format(_bucketName, str(bucketLogging), name, arn), method='a+')
+
+			if bucketLogging == None:
+				print(f"[!] {_bucketName}\t ~ {bucketLogging}\t ~ {name}\t ~ {arn}")
 
 			else:
-				print(f"[#] {_bucketName}\t ~ {deletionStatus}\t ~ {name}\t ~ {arn}")
+				print(f"[#] {_bucketName}\t ~ {bucketLogging}\t ~ {name}\t ~ {arn}")
 
 		except botocore.exceptions.ClientError:
 			print(f"[!!!] {_bucketName}\t ~ {name}\t ~ {arn} -- Access Denied")
 
 def main():
+	writeIntoFile(fileName, stdout="Bucket Name,Access Logging,Trail Name,Trail ARN\n", method='w+')
 	clientCall 	= authorizedClientCall()
+
 	listCloudtrailBuckets(clientCall)
 
 if __name__ == '__main__':
