a63d5817fc86d86f624374e8503787efdac35c49
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-15 16:22:16 +0500

    Implemented concurrency and outputting into a file

--- Cloudformation/stackTerminationProtection.py
+++ Cloudformation/stackTerminationProtection.py
@@ -4,26 +4,37 @@ from jmespath import search as queryJson
 import botocore
 import boto3
 import json
+import concurrent.futures
+
+fileName 	= "Cloudformation_Stacks_without_TerminationProtection.csv"
+
+def writeIntoFile(filename, stdout, method='w+'):
+	with open(filename, method) as f: f.write(stdout)
 
 def authorizedClientCall():
 	client 		= boto3.client("cloudformation")
 	return(client)
 
+clientCall 	= authorizedClientCall()
+
 def parseJson(query, jsonObj):
 	return(queryJson(query, jsonObj))
 
-def listCloudformationStacks(clientApiCall):
-	fetchStacks 	= clientApiCall.describe_stacks()
+def listCloudformationStacks():
+	fetchStacks 	= clientCall.describe_stacks()
 	jsonData 		= parseJson('Stacks[*].StackName', fetchStacks)
 	return(jsonData)
 
-def getStackTerminationProtection(_stackName, clientApiCall):
-	fetchData 	= clientApiCall.describe_stacks(StackName=_stackName)
+def getStackTerminationProtection(_stackName):
+	fetchData 	= clientCall.describe_stacks(StackName=_stackName)
 	jsonData 	= parseJson('Stacks[*]', fetchData)
+
 	termProtec 	= parseJson('[].EnableTerminationProtection', jsonData)
 	StackId 	= parseJson('[].StackId', jsonData)
 
 	for ids, stackProtection in zip(StackId, termProtec):
+		writeIntoFile(fileName, stdout="{},{},{}\n".format(_stackName, stackProtection, ids), method='a+')
+
 		if stackProtection == False:
 			print(f"[!] {_stackName}\t~ {stackProtection}\t~ {ids}")
 
@@ -31,13 +42,13 @@ def getStackTerminationProtection(_stackName, clientApiCall):
 			print(f"[@] {_stackName}\t~ {stackProtection}\t~ {ids}")
 
 def main():
-	clientCall 	= authorizedClientCall()
-	stacks 		= listCloudformationStacks(clientCall)
+	writeIntoFile(fileName, stdout="StackName,Termination Protection,Stack ID\n", method='w+')
+	stacks 		= listCloudformationStacks()
 
 	print("[#] StackName\t\t~ TerminationProtection\t~ Stack ID")
 
-	for _stacks in stacks:
-		getStackTerminationProtection(_stacks, clientCall)
+	with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+		executor.map(getStackTerminationProtection, stacks)
 
 if __name__ == '__main__':
 	main()
