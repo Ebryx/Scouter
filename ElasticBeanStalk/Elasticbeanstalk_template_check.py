8fbd6d042fa9b261e60d6433afaf2fb35adc6891
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-15 15:56:11 +0500

    Fixed `NoneType` return on no `_objs` in a bucket

--- ElasticBeanStalk/Elasticbeanstalk_template_check.py
+++ ElasticBeanStalk/Elasticbeanstalk_template_check.py
@@ -23,24 +23,27 @@ def getTemplateContents(buckets):
 	bucket_names_list	= search('Buckets[*].Name', bucketsList)
 	pattern				= 'elasticbeanstalk*'
 	templates 			= fnmatch.filter(bucket_names_list, pattern)
-	
+
 	if templates:
 		for buckets in templates:
 			print(f"[#] Bucket: {buckets}")
+			
 			objects 	= search('Contents[*].Key', s3.list_objects(Bucket=buckets))
+			
+			if objects:
+				for _objs in objects:	
+					if "resources/templates/" in _objs:
+						print(_objs)
+						appName.append(_objs.split("/")[2])
+						tempList.append(f"{templates[0]}/{_objs}")
 
-			for _objs in objects:	
-				if "resources/templates/" in _objs:
-					appName.append(_objs.split("/")[2])
-					tempList.append(f"{templates[0]}/{_objs}")
+				for templates in tempList:
+					print(f"[#] Application Name: {appName[0]}")
+					print(f"[#] Template Path: {templates}")
 
-			for templates in tempList:
-				print(f"[#] Application Name: {appName[0]}")
-				print(f"[#] Template Path: {templates}")
-
-				templateName 	= templates.replace(f'{buckets}/', '')
-				_object 		= s3.get_object(Bucket=buckets, Key=templateName)
-				print(f"[~] Template Contents: \n```\n{search('Body', _object).read().decode()}```\n")
+					templateName 	= templates.replace(f'{buckets}/', '')
+					_object 		= s3.get_object(Bucket=buckets, Key=templateName)
+					print(f"[~] Template Contents: \n```\n{search('Body', _object).read().decode()}```\n")
 	
 	else:
 		print("[!] Couldn't find a bucket with elasticbeanstalk as perfix, so most probably there are no elasticbeanstalk configuration templates.")
