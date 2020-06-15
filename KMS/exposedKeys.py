770af8db94359b228f08c9a3ee824008e7483559
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-15 17:02:53 +0500

    Made script modular and implemented concurrency

--- KMS/exposedKeys.py
+++ KMS/exposedKeys.py
@@ -2,28 +2,14 @@ from pygments import highlight, lexers, formatters
 import subprocess
 import json
 import re
-
-
-###############
-## User Input
+import concurrent.futures
 
 fileName 	= "KMS_exposed_keys.d"
-
-###############
 
 def writeIntoFile(filename, stdout, method='w+'):
 	with open(filename, method) as f: f.write(stdout)
 
-command 	= ["aws", "kms", "list-aliases"]
-_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err 	= _subp.communicate()
-out 		= json.loads(out)['Aliases']
-
-writeIntoFile(fileName, stdout="[~] Please find keys with JSON data [  Principal -> AWS ->  * ] yourself (since I'm not that intelligent :'( )\n\n", method='w+')
-print("[~] Please find keys with JSON data [  Principal -> AWS ->  * ] from the yourself (since I'm not that intelligent :'( )\n")
-
-for keys in out:
-
+def findExposedKeys(keys):
 	ARN 			= keys['AliasArn']
 	keyName 		= keys['AliasName'].replace("alias/", "")
 
@@ -37,16 +23,31 @@ for keys in out:
 		_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		out, err 	= _subp.communicate()
 
+		out 		= out.decode()
 		out 		= out.replace('\\"', '"').replace("\\n", "\n").replace('"{', "{").replace('}"', '}')
 		out 		= json.dumps(json.loads(out), indent=4)
-		
-		print("[#] TargetKey ~ {}\n[#] keyName ~ {}\n[#] ARN ~ {}\n[#] Json Object ~ \n{}\n\n\n\n".format(targetKeyID, keyName, ARN, highlight(unicode(out, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())))
+
+		print("[#] TargetKey ~ {}\n[#] keyName ~ {}\n[#] ARN ~ {}\n[#] Json Object ~ \n{}\n\n\n\n".format(targetKeyID, keyName, ARN, highlight(out.encode('utf-8'), lexers.JsonLexer(), formatters.TerminalFormatter())))
+
 		writeIntoFile(fileName, stdout="[#] TargetKey ~ {}\n[#] keyName ~ {}\n[#] ARN ~ {}\n[#] Json Object ~ \n{}\n\n\n".format(targetKeyID, keyName, ARN, out), method='a+')
 
 	except KeyboardInterrupt:
 		exit("[!] You killed my process :(")
 
-	except:
-		pass
+def main():
+	writeIntoFile(fileName, stdout="[~] Please find keys with JSON data [  Principal -> AWS ->  * ] yourself (since I'm not that intelligent :'( )\n\n", method='w+')
+	command 	= ["aws", "kms", "list-aliases"]
 
-print("[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err 	= _subp.communicate()
+	out 		= json.loads(out)['Aliases']
+
+	print("[~] Please find keys with JSON data [  Principal -> AWS ->  * ] yourself (since I'm not that intelligent :'( )\n")
+
+	with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+		executor.map(findExposedKeys, out)
+
+	print("[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+
+if __name__ == '__main__':
+	main()
