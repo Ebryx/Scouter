529ce236a163a1437d7582d28103ada4f67a317d
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-06-18 11:28:17 +0500

    Made modular and implemented concurrency

--- KMS/disabledKeys.py
+++ KMS/disabledKeys.py
@@ -1,45 +1,41 @@
 import subprocess
 import json
 import re
-
-
-###############
-## User Input
+import concurrent.futures
 
 fileName 	= "KMS_disabled_keys.csv"
-
-###############
 
 def writeIntoFile(filename, stdout, method='w+'):
 	with open(filename, method) as f: f.write(stdout)
 
-command 	= ["aws", "kms", "list-keys", "--query", "Keys[*].KeyId"]
-_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err 	= _subp.communicate()
-out 		= json.loads(out)
+def findKeyStatus(keys):
+	command 	= ["aws", "kms", "describe-key", "--key-id", keys]
+	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err 	= _subp.communicate()
+	params 		= json.loads(out)['KeyMetadata']
 
-writeIntoFile(fileName, stdout="KMS Key,Description,ARN,State\n", method='w+')
-print("[#] KMS Key ~ ARN ~ Description ~ State \n")
+	keyId 		= params['KeyId']
+	description	= params['Description']
+	arn 		= params['Arn']
+	state 		= params['KeyState']
 
-for keys in out:
-	try:
-		command 	= ["aws", "kms", "describe-key", "--key-id", keys]
-		_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err 	= _subp.communicate()
-		params 		= json.loads(out)['KeyMetadata']
+	print("[~] {} ~ {} ~ {} ~ {}".format(keyId, description, arn, state))
+	writeIntoFile(fileName, stdout="{},{},{},{}\n".format(keyId, description, arn, state), method='a+')
 
-		keyId 		= params['KeyId']
-		description	= params['Description']
-		arn 		= params['Arn']
-		state 		= params['KeyState']
+def main():
+	writeIntoFile(fileName, stdout="KMS Key,Description,ARN,State\n", method='w+')
+	command 	= ["aws", "kms", "list-keys", "--query", "Keys[*].KeyId"]
 
-		print("[~] {} ~ {} ~ {} ~ {}".format(keyId, description, arn, state))
-		writeIntoFile(fileName, stdout="{},{},{},{}\n".format(keyId, description, arn, state), method='a+')
+	_subp 		= subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err 	= _subp.communicate()
+	out 		= json.loads(out)
 
-	except KeyboardInterrupt:
-		exit("[!] You killed my process :(")
+	print("[#] KMS Key ~ ARN ~ Description ~ State \n")
 
-	except:
-		pass
+	with concurrent.futures.ProcessPoolExecutor(max_workers=30) as executor:
+		executor.map(findKeyStatus, out)
 
-print("[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+	print("[~] Script execution completed!\n[~] Data written in {}".format(fileName))
+
+if __name__ == '__main__':
+	main()
